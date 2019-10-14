ec4aa8d960dbbf97b301bcd35b16d524f383b09b
TIANCHENG <306489696@QQ.COM>
2019-10-14 16:12:32 -0700

    Added Road track
    brian added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log(other.name);
+        if (other.CompareTag("RoadTrigger")) {
+            RoadManager.instance.GenerateNewRoad();
+            other.GetComponent<RoadDestructor>().Destruct(1f);
+        }
+    }
+}
---
+++ Assets/Scripts/RoadController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class RoadController : MonoBehaviour {
+        
+    private void Update() {
+        transform.position = Vector3.MoveTowards(transform.position, 
+            RoadManager.instance.endPoint.position, 
+            RoadManager.instance.speed * Time.deltaTime);
+    }
+}
