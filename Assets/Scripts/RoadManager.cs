100fde76e1c96187459536c7a56f0e421c067ce4
Brian <bjjx1999@live.com>
2019-10-20 22:37:45 -0700

    Improve road generation

--- Assets/Scripts/RoadManager.cs
+++ Assets/Scripts/RoadManager.cs
@@ -11,29 +11,29 @@ public struct RoadInfo {
 
 public class RoadManager : MonoBehaviour {
     public float speed;
-    public RoadInfo[] RoadInfos;
+//    public RoadInfo[] RoadInfos;
     public GameObject[] roads;
 
     public static RoadManager instance;
 
-    private void Start() {
+    private void Awake() {
         if (instance == null) {
             instance = this;
         } else {
             Debug.LogError("Try to load two RoadManager.");
         }
 
-        foreach (var roadInfo in RoadInfos) {
-            foreach (var road in roadInfo.preBuild) {
-                road.GetComponent<RoadController>().Init(roadInfo.endPoint.position, speed);
-            }
-        }
+//        foreach (var roadInfo in RoadInfos) {
+//            foreach (var road in roadInfo.preBuild) {
+//                road.GetComponent<RoadSegmentController>().Init(roadInfo.endPoint.position, speed);
+//            }
+//        }
     }
 
-    public void GenerateNewRoadSegment() {
-        foreach (var roadInfo in RoadInfos) {
-            var road = Instantiate(roads[Random.Range(0, roads.Length)], roadInfo.spawnPoint.position, Quaternion.identity);
-            road.GetComponent<RoadController>().Init(roadInfo.endPoint.position, speed);
-        }
-    }
+//    public void GenerateNewRoadSegment() {
+//        foreach (var roadInfo in RoadInfos) {
+//            var road = Instantiate(roads[Random.Range(0, roads.Length)], roadInfo.spawnPoint.position, Quaternion.identity);
+//            road.GetComponent<RoadSegmentController>().Init(roadInfo.endPoint.position, speed);
+//        }
+//    }
 }
