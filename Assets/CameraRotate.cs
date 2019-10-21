0f0e60cf748130bb60fbc8db170e8ba60fa571b5
Sicilia <huizim@uci.edu>
2019-10-21 15:36:21 -0700

    modify

---
+++ Assets/CameraRotate.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    public float RotateAngle = 5;
+    public float RotateSpeed = 10;
+    public float RotateBackSpeed = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            StopAllCoroutines();
+            gameObject.transform.rotation = Quaternion.Euler(0, 90, 0);
+            StartCoroutine(RotateImage(RotateAngle, RotateSpeed, RotateBackSpeed));
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            StopAllCoroutines();
+            gameObject.transform.rotation = Quaternion.Euler(0, 90, 0);
+            StartCoroutine(RotateImage(-RotateAngle, RotateSpeed, RotateBackSpeed));
+        }
+
+    }
+
+    IEnumerator RotateImage(float Angle, float RotateSpeed, float RotateBackSpeed)
+    {
+        while (gameObject.transform.rotation != Quaternion.Euler(0, 90, Angle))
+        {
+            gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation, Quaternion.Euler(0, 90, Angle), 1/RotateSpeed);
+            yield return null;
+        }
+
+        while (gameObject.transform.rotation != Quaternion.Euler(0, 90, 0))
+        {
+            gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation, Quaternion.Euler(0, 90, 0), 1/RotateBackSpeed);
+            yield return null;
+        }
+
+        gameObject.transform.rotation = Quaternion.Euler(0, 90, 0);
+        yield return null;
+    }
+}
--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -1,47 +1,74 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class CameraShake : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+//public class CameraShake : MonoBehaviour
+//{
+//    public float shakeAmount;//The amount to shake this frame.
+//    public float shakeDuration;//The duration this frame.
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            gameObject.transform.rotation = Quaternion.Euler(0, 90, 0);
-            StartCoroutine(RotateImage(5, 0.1f));
-        }
+//    //Readonly values...
+//    float shakePercentage;//A percentage (0-1) representing the amount of shake to be applied when setting rotation.
+//    float startAmount;//The initial shake amount (to determine percentage), set when ShakeCamera is called.
+//    float startDuration;//The initial shake duration, set when ShakeCamera is called.
 
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            gameObject.transform.rotation = Quaternion.Euler(0, 90, 0);
-            StartCoroutine(RotateImage(-5, 0.1f));
-        }
-    
-    }
+//    bool isRunning = false; //Is the coroutine running right now?
 
-    IEnumerator RotateImage(float Angle, float moveSpeed)
-    {
-        while (gameObject.transform.rotation != Quaternion.Euler(0, 90, Angle))
-        {
-            gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation, Quaternion.Euler(0, 90, Angle), moveSpeed);
-            yield return null;
-        }
+//    public bool smooth;//Smooth rotation?
+//    public float smoothAmount = 5f;//Amount to smooth
 
-        while (gameObject.transform.rotation != Quaternion.Euler(0, 90, 0))
-        {
-            gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation, Quaternion.Euler(0, 90, 0), 0.2f);
-            yield return null;
-        }
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        ShakeCamera();
+//    }
 
-        gameObject.transform.rotation = Quaternion.Euler(0, 90, 0);
-        yield return null;
-    }
-}
+//    // Update is called once per frame
+//    void Update()
+//    {
+
+//    }
+
+//    void ShakeCamera() {
+ 
+//		startAmount = shakeAmount;//Set default (start) values
+//		startDuration = shakeDuration;//Set default (start) values
+ 
+//		if (!isRunning) StartCoroutine (Shake());//Only call the coroutine if it isn't currently running. Otherwise, just set the variables.
+//	}
+ 
+//	public void ShakeCamera(float amount, float duration) {
+ 
+//		shakeAmount += amount;//Add to the current amount.
+//		startAmount = shakeAmount;//Reset the start amount, to determine percentage.
+//		shakeDuration += duration;//Add to the current time.
+//		startDuration = shakeDuration;//Reset the start time.
+ 
+//		if(!isRunning) StartCoroutine (Shake());//Only call the coroutine if it isn't currently running. Otherwise, just set the variables.
+//	}
+ 
+ 
+//	IEnumerator Shake() {
+//		isRunning = true;
+ 
+//		while (true) {
+//			Vector3 rotationAmount = Random.insideUnitSphere * shakeAmount;//A Vector3 to add to the Local Rotation
+//			rotationAmount.z = 0;//Don't change the Z; it looks funny.
+ 
+//			shakePercentage = shakeDuration / startDuration;//Used to set the amount of shake (% * startAmount).
+ 
+//			//shakeAmount = startAmount * shakePercentage;//Set the amount of shake (% * startAmount).
+//			shakeDuration = Mathf.Lerp(shakeDuration, 0, Time.deltaTime);//Lerp the time, so it is less and tapers off towards the end.
+ 
+ 
+//			if(smooth)
+//				transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(rotationAmount), Time.deltaTime * smoothAmount);
+//			else
+//				transform.localRotation = Quaternion.Euler (rotationAmount);//Set the local rotation the be the rotation amount.
+ 
+//			yield return null;
+//		}
+//		transform.localRotation = Quaternion.identity;//Set the local rotation to 0 when done, just to get rid of any fudging stuff.
+//		isRunning = false;
+//	}
+//}
