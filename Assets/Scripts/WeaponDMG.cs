e3b6b36c60f91763887979e4f2f11023e73f918e
TIANCHENG <306489696@QQ.COM>
2019-10-20 20:42:29 -0700

    Sword collision detection added and enemy destroy and dying effecct
    First Version(without character health system)

---
+++ Assets/Scripts/WeaponDMG.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDMG : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if "move" in playercontroller is True
+        if (other.gameObject.tag == "Enemy") {
+            Destroy(other.gameObject.GetComponent<MeshRenderer>());
+            other.gameObject.transform.GetChild(0).gameObject.GetComponent<ParticleSystem>().Play();
+            Destroy(other.gameObject,1);// destroy the Enemy and play destroy deconstruction animation;
+
+        } 
+        //if "move" in playercontroller is False
+        //gameover, pause the game
+    }
+}
