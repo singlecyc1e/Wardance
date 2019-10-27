9cdf25b4570997b9f5f6f46c2f1c5c034baa4cac
Brian <bjjx1999@live.com>
2019-10-26 22:14:05 -0700

    Working on enemy generation

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        var levelInfo = DataUtility.GetLevelInfo(1);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
