9902fcf7adce6f6305e043c14ac2486c9b0525f3
Wayne Huang <huangw10@uci.edu>
2019-11-13 01:25:57 -0800

    1231
    fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,25 +1,9 @@
-<<<<<<< Updated upstream
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerController : MonoBehaviour
-{
-    private void OnTriggerEnter(Collider other) {
-        Debug.Log(other.name);
-        if (other.CompareTag("RoadTrigger")) {
-            RoadManager.instance.GenerateNewRoad();
-            other.GetComponent<RoadDestructor>().Destruct(1f);
-        }
-    }
-}
-=======
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+
 
 public enum SwipeDirection
 {
@@ -249,4 +233,3 @@ public class PlayerController : MonoBehaviour
 //    }
 
 }
->>>>>>> Stashed changes
