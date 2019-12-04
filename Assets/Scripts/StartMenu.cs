c5c9992548d2a5143afa67d0ed6d2af30a6c65c0
TIANCHENG <306489696@QQ.COM>
2019-12-03 21:22:34 -0800

    Add start menu

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public static StartMenu instance;
+    public bool STARTmenu = true;
+
+    private void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Start Menu");
+        if (objs.Length > 1) { Destroy(this.gameObject); }
+        DontDestroyOnLoad(this.gameObject);
+
+        if (instance == null) { instance = this; }
+        
+        if (STARTmenu) {
+            this.transform.GetChild(0).gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    private void BackToMenu()
+    {
+        STARTmenu = true;
+        this.transform.GetChild(0).gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void MyStart()
+    {
+        Debug.Log("mystart!");
+        STARTmenu = false;
+        this.transform.GetChild(0).gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Credits()
+    {
+        //display
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
