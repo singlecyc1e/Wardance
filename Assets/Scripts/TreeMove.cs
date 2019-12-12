842480edc605e9219d4cdc2d33eec4a89cb6bc9e
Brian <bjjx1999@live.com>
2019-12-11 16:45:39 -0800

    Integrate

--- Assets/Scripts/TreeMove.cs
+++ Assets/Scripts/TreeMove.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,8 +7,15 @@ public class TreeMove : MonoBehaviour
 {
     public float speed = 8f;
     // Update is called once per frame
-    void Update()
-    {
+
+    // private void Start() {
+    //     for (var i = 0; i < transform.childCount; i++) {
+    //         Destroy(transform.GetChild(i).gameObject, 5f);
+    //     }
+    // }
+
+    void Update() {
+        speed = RoadManager.instance.currentSpeed;
         transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x-1 , transform.position.y, transform.position.z ), speed * Time.deltaTime);
     }
 }
