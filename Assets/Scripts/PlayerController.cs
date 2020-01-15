7bed82609da9ee9acc1ba4442e59e2c0c3cab829
Brian <bjjx1999@live.com>
2020-01-15 10:55:19 -0800

    Fix touch

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,27 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Lean.Touch;
 using UnityEngine;
 
-public enum SwipeDirection
-{
-    None, Left, Right
+public enum SwipeDirection {
+    None,
+    Left,
+    Right
 }
-enum PlayerCommand
-{
+
+enum PlayerCommand {
     idle = 0,
     Leftswing = 1,
     Rightswing = 2
 }
 
 public enum Direction {
-    None, Up, Left, Down, Right
+    None,
+    Up,
+    Left,
+    Down,
+    Right
 }
 
 public struct FingerStorage {
@@ -36,8 +42,7 @@ public struct FingerStorage {
     }
 }
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     public float duration;
 
     public bool moving;
@@ -48,7 +53,7 @@ public class PlayerController : MonoBehaviour
     private Animator AnimeC;
     public float distance = 4f;
     public bool idleTimeup;
-    
+
     public AudioClip[] SwipeSound;
 
     private AudioSource audiosource;
@@ -61,61 +66,99 @@ public class PlayerController : MonoBehaviour
     //    private bool hasStashInput;
     private SwipeDirection stashedDirection;
 
-    private Dictionary<int, FingerStorage> storedTouches;
+    // private Dictionary<int, FingerStorage> storedTouches;
+    private FingerStorage fingerStorage;
 
     public TimeController TimeManager;
     public static PlayerController instance;
 
-    private void Awake()
-    {
-        if (instance == null)
-        {
+    private void Awake() {
+        if (instance == null) {
             instance = this;
         }
     }
 
-    private void Start()
-    {
+    private void Start() {
         AnimeC = GameObject.Find("Sword").GetComponent<Animator>();
         PlayerCamera = GameObject.Find("Main Camera");
         OldPosition = gameObject.transform.position;
         audiosource = GetComponent<AudioSource>();
+        
+        fingerStorage = new FingerStorage(Direction.None, 5);
+    }
+
+    private void UpdateTouch() {
+        if(LeanTouch.Fingers.Count == 0) return;
+        
+        var deltaPosition = Lean.Touch.LeanGesture.GetScreenDelta();
+        var angle = Vector2.SignedAngle(Vector2.up, deltaPosition);
+        var directionDetected = Direction.None;
+        if (angle >= -45 && angle < 45) {
+            directionDetected = Direction.Up;
+        } else if (angle >= 45 && angle < 135) {
+            directionDetected = Direction.Right;
+        } else if (angle >= 135 || angle < -135) {
+            directionDetected = Direction.Down;
+        } else if (angle >= -135 && angle < -45) {
+            directionDetected = Direction.Left;
+        }
+        
+        Debug.Log("Detect: " + directionDetected);
+
+        switch (directionDetected == fingerStorage.direction) {
+            case true:
+                fingerStorage.DecreaseCounter();
+                break;
+            default:
+                fingerStorage = new FingerStorage(directionDetected, 5);
+                break;
+        }
+
+        if (fingerStorage.counter <= 0) {
+            switch (fingerStorage.direction) {
+                case Direction.None:
+                    break;
+                case Direction.Up:
+                    OnUpSwipe();
+                    break;
+                case Direction.Left:
+                    OnRightSwipe();
+                    break;
+                case Direction.Down:
+                    OnDownSwipe();
+                    break;
+                case Direction.Right:
+                    OnLeftSwipe();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            fingerStorage.ResetCounter();
+        }
     }
 
 
-    private void Update()
-    {
-#if UNITY_STANDALONE
-
-        if (Input.GetKeyDown(KeyCode.A))
-        {
+    private void Update() {
+#if UNITY_STANDALONE || UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.A)) {
             OnLeftSwipe();
-
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
+        } else if (Input.GetKeyDown(KeyCode.D)) {
             OnRightSwipe();
-
         }
 
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-
-            if (WeaponDMG.instance.BulletTime)
-            {
+        if (Input.GetKeyDown(KeyCode.B)) {
+            if (WeaponDMG.instance.BulletTime) {
                 WeaponDMG.instance.BulletTime = false;
-            }
-            else
-            {
+            } else {
                 WeaponDMG.instance.BulletTime = true;
             }
+
             TimeManager.BulletTime();
-                       
         }
 
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
+        if (Input.GetKeyDown(KeyCode.Space)) {
             OnUpSwipe();
         }
 
@@ -123,82 +166,13 @@ public class PlayerController : MonoBehaviour
             OnDownSwipe();
         }
 #endif
-
-        foreach (var touch in Input.touches) {
-            switch (touch.phase) {
-                case TouchPhase.Began:
-                    storedTouches[touch.fingerId] = new FingerStorage(Direction.None, 5);
-                    break;
-                case TouchPhase.Moved:
-                    var deltaPosition = touch.deltaPosition;
-                    var angle = Vector2.SignedAngle(Vector2.up, deltaPosition);
-                    var directionDetected = Direction.None;
-                    if (angle >= -45 && angle < 45) {
-                        directionDetected = Direction.Up;
-                    } else if (angle >= 45 && angle < 135) {
-                        directionDetected = Direction.Right;
-                    } else if (angle >= 135 || angle < -135) {
-                        directionDetected = Direction.Down;
-                    } else if (angle >= -135 && angle < -45) {
-                        directionDetected = Direction.Down;
-                    }
-
-                    switch (directionDetected == storedTouches[touch.fingerId].direction) {
-                        case true:
-                            // var newStorage = new FingerStorage(storedTouches[touch.fingerId]);
-                            storedTouches[touch.fingerId].DecreaseCounter();
-                            break;
-                        default:
-                            storedTouches[touch.fingerId] = new FingerStorage(directionDetected, 5);
-                            break;
-                    }
-
-                    foreach (var storedTouch in storedTouches) {
-                        if (storedTouch.Value.counter <= 0) {
-                            switch (storedTouch.Value.direction) {
-                                case Direction.None:
-                                    break;
-                                case Direction.Up:
-                                    OnUpSwipe();
-                                    break;
-                                case Direction.Left:
-                                    OnLeftSwipe();
-                                    break;
-                                case Direction.Down:
-                                    OnDownSwipe();
-                                    break;
-                                case Direction.Right:
-                                    OnRightSwipe();
-                                    break;
-                                default:
-                                    throw new ArgumentOutOfRangeException();
-                            }
-                            
-                            storedTouch.Value.ResetCounter();
-                        }
-                    }
-                    break;
-                case TouchPhase.Stationary:
-                    break;
-                case TouchPhase.Ended:
-                    storedTouches.Remove(touch.fingerId);
-                    break;
-                case TouchPhase.Canceled:
-                    storedTouches.Remove(touch.fingerId);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
+        
+        UpdateTouch();
     }
 
-    private void FixedUpdate()
-    {
-
-        if (LastCommand != PlayerCommand.idle)
-        {
-            if (Time.time - LastCommandTime >= 1.1f && Time.time - LastCommandTime <= 1.2f )
-            {
+    private void FixedUpdate() {
+        if (LastCommand != PlayerCommand.idle) {
+            if (Time.time - LastCommandTime >= 1.1f && Time.time - LastCommandTime <= 1.2f) {
                 AnimeC.ResetTrigger("Left to Right");
                 AnimeC.ResetTrigger("Right to Left");
                 AnimeC.ResetTrigger("LS");
@@ -232,48 +206,38 @@ public class PlayerController : MonoBehaviour
                     throw new ArgumentOutOfRangeException();
             }
         }
-
-
     }
 
-    private void ClearStash()
-    {
+    private void ClearStash() {
         stashedDirection = SwipeDirection.None;
     }
 
-    public void OnLeftSwipe()
-    {
-
+    public void OnLeftSwipe() {
         if ((transform.position.z > OldPosition.z)) return;
 
         audiosource.clip = SwipeSound[UnityEngine.Random.Range(0, 3)];
         audiosource.Play();
         PlayerCamera.GetComponent<CameraShake>().CameraLeftSwipt();
 
-        if (moving)
-        {
+        if (moving) {
             stashedDirection = SwipeDirection.Left;
             Invoke(nameof(ClearStash), 0.5f);
             return;
         }
 
 
-        if (LastCommand == PlayerCommand.Rightswing)
-        {
+        if (LastCommand == PlayerCommand.Rightswing) {
             AnimeC.SetBool("idle", false);
             AnimeC.ResetTrigger("RS");
             AnimeC.ResetTrigger("Left to Right");
             AnimeC.SetTrigger("Right to Left");
             //Debug.Log("R TO L");
-        }
-        else
-        {
+        } else {
             AnimeC.SetBool("idle", false);
             AnimeC.ResetTrigger("Right to Left");
             AnimeC.ResetTrigger("LS");
             AnimeC.ResetTrigger("RS");
             AnimeC.SetTrigger("LS");
-
         }
 
         LastCommand = PlayerCommand.Leftswing;
@@ -281,11 +245,9 @@ public class PlayerController : MonoBehaviour
         moving = true;
         startTime = Time.time;
         targetZ = transform.position.z + distance;
-        
     }
 
-    public void OnRightSwipe()
-    {
+    public void OnRightSwipe() {
         //Debug.Log(transform.position.z);
         if (transform.position.z < -distance) return;
 
@@ -293,27 +255,23 @@ public class PlayerController : MonoBehaviour
         audiosource.Play();
         PlayerCamera.GetComponent<CameraShake>().CameraRightSwipe();
 
-        if (moving)
-        {
+        if (moving) {
             stashedDirection = SwipeDirection.Right;
             Invoke(nameof(ClearStash), 0.5f);
             return;
         }
-        if (LastCommand == PlayerCommand.Leftswing)
-        {
+
+        if (LastCommand == PlayerCommand.Leftswing) {
             AnimeC.SetBool("idle", false);
             AnimeC.ResetTrigger("LS");
             AnimeC.ResetTrigger("Right to Left");
             AnimeC.SetTrigger("Left to Right");
-        }
-        else
-        {
+        } else {
             AnimeC.SetBool("idle", false);
             AnimeC.ResetTrigger("Left to Right");
             AnimeC.ResetTrigger("RS");
             AnimeC.ResetTrigger("LS");
             AnimeC.SetTrigger("RS");
-
         }
 
         LastCommand = PlayerCommand.Rightswing;
@@ -323,8 +281,7 @@ public class PlayerController : MonoBehaviour
         targetZ = transform.position.z - distance;
     }
 
-    public void OnDownSwipe()
-    {
+    public void OnDownSwipe() {
         if (slashing || gameObject.transform.position.y < 1.0f)
             return;
 
@@ -342,31 +299,27 @@ public class PlayerController : MonoBehaviour
         StartCoroutine(WaitForSlash());
     }
 
-    public void OnUpSwipe()
-    {
+    public void OnUpSwipe() {
         gameObject.GetComponent<PlayerJump>().JumpUpSwipe();
     }
 
-    IEnumerator WaitForSlash()
-    {
+    IEnumerator WaitForSlash() {
         yield return new WaitForSeconds(0.25f);
         slashing = false;
     }
 
-    public IEnumerator ShakeBody(float duration, float magnitude)
-    {
+    public IEnumerator ShakeBody(float duration, float magnitude) {
         Debug.Log("shake");
         Vector3 originalPos = transform.position;
         float elapsed = 0.0f;
-        while (elapsed < duration)
-        {
+        while (elapsed < duration) {
             float z = UnityEngine.Random.Range(-1f, 1f) * magnitude;
             float y = UnityEngine.Random.Range(0, 1f) * magnitude;
             transform.localPosition = new Vector3(originalPos.x, y, z);
             elapsed += Time.deltaTime;
             yield return null;
         }
+
         transform.localPosition = originalPos;
     }
-
 }
