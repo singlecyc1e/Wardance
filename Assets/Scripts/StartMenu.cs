12aea2f0bc75620b9e508b0bf4b623d362f1f783
Wayne Huang <huangw10@uci.edu>
2020-01-26 17:52:05 -0800

    new changes of startmenu
    fog start with new effect

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -7,20 +7,41 @@ public class StartMenu : MonoBehaviour
 {
     public static StartMenu instance;
     public bool STARTmenu = true;
+    public GameObject UI;
+    public Animator myAnimationController;
 
     private void Awake()
     {
+        UI = GameObject.FindGameObjectWithTag("UI");
         GameObject[] objs = GameObject.FindGameObjectsWithTag("Start Menu");
         if (objs.Length > 1) { Destroy(this.gameObject); }
         DontDestroyOnLoad(this.gameObject);
 
         if (instance == null) { instance = this; }
-        
         if (STARTmenu) {
             this.transform.GetChild(0).gameObject.SetActive(true);
-            Time.timeScale = 0;
+            StartCoroutine(ExampleCoroutine());
+            if (UI != null)
+                UI.SetActive(false);
+
         }
     }
+
+    IEnumerator ExampleCoroutine()
+    {
+        yield return new WaitForSeconds(0.1f);
+        Time.timeScale = 0;
+    }
+
+    IEnumerator Fogstart()
+    {
+        yield return new WaitForSecondsRealtime(2f);
+        if (UI != null)
+            UI.SetActive(true);
+        this.transform.GetChild(0).gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
 
     public void BackToMenu()
     {
@@ -32,8 +53,9 @@ public class StartMenu : MonoBehaviour
     public void MyStart()
     {
         STARTmenu = false;
+        myAnimationController.SetBool("move", true);
+        StartCoroutine(Fogstart());
         this.transform.GetChild(0).gameObject.SetActive(false);
-        Time.timeScale = 1;
     }
 
     public void Credits()
