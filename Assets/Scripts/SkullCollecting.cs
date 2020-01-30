950b8f539d1f283847ec2f4537ca3eb8dd0d1f1e
TIANCHENG <306489696@QQ.COM>
2020-01-29 16:48:51 -0800

    try last one
    1/29

---
+++ Assets/Scripts/SkullCollecting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullCollecting : MonoBehaviour
+{
+    public GameObject Skull;
+    public Transform targetPosition;
+    public int skullnum = 5;
+
+    public void CreateSkull()
+    {
+        for (int num = 0; num < skullnum; num++)
+        {
+            GameObject.Instantiate(Skull, this.transform); //assign the targetPosition to the skull 
+
+            /// question 1. should i instantiate gameobjects or just images?
+            /// when adds to num left top, the num will shake from small to big
+        }
+        
+    }
+    
+}
--- Assets/Scripts/WeaponDMG.cs
+++ Assets/Scripts/WeaponDMG.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using EZCameraShake;
+using TMPro;
 
 public class WeaponDMG : MonoBehaviour
 {
@@ -12,6 +13,11 @@ public class WeaponDMG : MonoBehaviour
     public bool Alive = true;
     public TimeController timeManager;
     public bool BulletTime = false;
+    public TextMeshProUGUI chineseword;
+    public float displayduration = .2f;
+    public float timecheck = 1.5f;
+    private float killtime = 0f;
+    private int counter = 1;
     //public TimeController Timemanager;
     // Start is called before the first frame update
 
@@ -23,9 +29,11 @@ public class WeaponDMG : MonoBehaviour
         }
     }
 
+
     private void Start()
     {
         Alive = true;
+        chineseword.enabled = false;
     }
 
     public void SetupDeathMenu() {
@@ -68,10 +76,12 @@ public class WeaponDMG : MonoBehaviour
             //if "move" in playercontroller is True
             if (PlayerController.instance.moving | RageSystem.instance.RageState | PlayerController.instance.slashing)
             {
+                WordDisplay();
+                killtime = Time.time;
                 killscore += 1;
-                UI_killscore.text = killscore.ToString() + "人斩";
+                UI_killscore.text = "x " + killscore.ToString();
                 Destroy(other.gameObject.GetComponent<MeshRenderer>());
-                other.gameObject.transform.GetChild(0).gameObject.GetComponent<ParticleSystem>().Play();
+                other.gameObject.transform.GetChild(0).GetChild(0).gameObject.GetComponent<ParticleSystem>().Play();
                 Destroy(other.gameObject, 3);// destroy the Enemy and play destroy deconstruction animation;
                 RageSystem.instance.AddRageValue();
                 if (BulletTime)
@@ -91,10 +101,12 @@ public class WeaponDMG : MonoBehaviour
             //if "move" in playercontroller is True
             if (RageSystem.instance.RageState)
             {
+                WordDisplay();
+                killtime = Time.time;
                 killscore += 1;
-                UI_killscore.text = killscore.ToString() + "人斩";
+                UI_killscore.text = "x " + killscore.ToString();
                 Destroy(other.gameObject.GetComponent<MeshRenderer>());
-                other.gameObject.transform.GetChild(0).gameObject.GetComponent<ParticleSystem>().Play();
+                other.gameObject.transform.GetChild(0).GetChild(0).gameObject.GetComponent<ParticleSystem>().Play();
                 Destroy(other.gameObject, 3);// destroy the Enemy and play destroy deconstruction animation;
                 RageSystem.instance.AddRageValue();
                 if (BulletTime)
@@ -109,5 +121,93 @@ public class WeaponDMG : MonoBehaviour
         }
         //if "move" in playercontroller is False
         //gameover, pause the game
+    }
+
+    private void WordDisplay()
+    {
+        if ((Time.time - killtime) < timecheck)
+        {
+            counter++;
+            StartCoroutine(Display());
+        }
+        else
+        {
+            counter = 1;
+        }
+    }
+
+    IEnumerator Display()
+    {
+        chineseword.enabled = true;
+        if (counter == 0)
+        {
+            chineseword.text = "";
+        }
+        else if (counter == 1)
+        {
+            chineseword.text = "";
+        }
+        else if (counter == 2)
+        {
+            chineseword.text = "贰连斩";
+        }
+        else if (counter == 3)
+        {
+            chineseword.text = "叁连斩";
+        }
+        else if (counter == 4)
+        {
+            chineseword.text = "肆连斩";
+        }
+        else if (counter == 5)
+        {
+            chineseword.text = "伍连斩";
+        }
+        else if (counter == 6)
+        {
+            chineseword.text = "陆连斩";
+        }
+        else if (counter == 7)
+        {
+            chineseword.text = "柒连斩";
+        }
+        else if (counter == 8)
+        {
+            chineseword.text = "超凡入圣";
+        }
+        else if (counter == 9)
+        {
+            chineseword.text = "神挡杀神";
+        }
+        else if (counter == 10)
+        {
+            chineseword.text = "佛挡杀佛";
+        }
+        else if (counter == 11)
+        {
+            chineseword.text = "吾即天命";
+        }
+        else if (counter == 12)
+        {
+            chineseword.text = "拾贰连斩";
+        }
+        else if (counter == 13)
+        {
+            chineseword.text = "拾叁连斩";
+        }
+        else if (counter == 14)
+        {
+            chineseword.text = "拾肆连斩";
+        }
+        else if (counter == 15)
+        {
+            chineseword.text = "拾伍连斩";
+        }
+        chineseword.rectTransform.Rotate(0.0f, 0.0f, UnityEngine.Random.Range(-10.0f, 10.0f));
+        yield return new WaitForSeconds(displayduration);
+        chineseword.rectTransform.rotation = Quaternion.identity;
+        chineseword.text = "";
+        chineseword.enabled = false;
+
     }
 }
