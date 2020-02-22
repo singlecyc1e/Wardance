a875c813023329309c700c2084fdabf58e88cc51
TIANCHENG <306489696@QQ.COM>
2020-02-22 13:41:13 -0800

    distance test

--- Assets/Scripts/DistanceDisplay.cs
+++ Assets/Scripts/DistanceDisplay.cs
@@ -7,14 +7,18 @@ public class DistanceDisplay : MonoBehaviour
     // level 1 28 sec
     // Start is called before the first frame update
     public Transform Endposition;
+    public float level1 = 20f;
+    private float overallSpeed = 0f;
     void Start()
     {
-        
+        Transform originalPosition = this.transform;
+        overallSpeed = ((Endposition.position.x - originalPosition.position.x) * Time.deltaTime/ level1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(overallSpeed);
+        this.transform.position = new Vector3(this.transform.position.x + overallSpeed, this.transform.position.y, this.transform.position.z);
     }
 }
---
+++ Assets/Scripts/timedisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timedisplay : MonoBehaviour
+{
+    private Text mytime;
+    private void Start()
+    {
+        mytime = this.transform.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mytime.text = Time.time.ToString();
+    }
+}
