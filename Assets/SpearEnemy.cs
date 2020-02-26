d0fc7412ee5ecdc418aa6df3e83f98ed1270a42e
fdsfa <fdaf>
2020-02-26 01:53:43 -0800

    spear enemy rework

---
+++ Assets/SpearEnemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearEnemy : MonoBehaviour
+{
+    public Animator SpearAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "HintTrigger")
+        {
+            SpearAnimator.SetTrigger("Start");
+        }
+    }
+}
