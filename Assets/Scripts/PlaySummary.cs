8b9a1e32c35392cad8850d105b045802f2c9b1c4
Sicilia Miao <huizimiao@gmail.com>
2020-03-08 06:31:11 -0700

    Butt

---
+++ Assets/Scripts/PlaySummary.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaySummary : MonoBehaviour
+{
+    public GameObject KillText;
+    public GameObject DistanceText;
+    public GameObject StartButtom;
+    public GameObject SummaryButtom;
+    public static bool IsStartMenu = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("BestScore"))
+        {
+            PlayerPrefs.SetInt("BestScore", 0);
+        }
+
+        if (!PlayerPrefs.HasKey("BestDistance"))
+        {
+            PlayerPrefs.SetInt("BestDistance", 0);
+        }
+
+        KillText.transform.GetChild(0).gameObject.SetActive(false);
+        DistanceText.transform.GetChild(0).gameObject.SetActive(false);
+
+        if (IsStartMenu == true)
+        {
+            IsStartMenu = false;
+            SummaryButtomCall();
+        }
+        else
+        {
+            DisplayPlaySummary();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DisplayPlaySummary()
+    {
+        StartButtom.SetActive(false);
+
+        KillText.GetComponent<Text>().text = PlayerPrefs.GetInt("CurrentScore").ToString() + " Kills";
+        DistanceText.GetComponent<Text>().text = PlayerPrefs.GetInt("CurrentDistance").ToString() + "m";
+
+        //Update best scores
+        if(PlayerPrefs.GetInt("CurrentScore") > PlayerPrefs.GetInt("BestScore"))
+        {
+            PlayerPrefs.SetInt("BestScore", PlayerPrefs.GetInt("CurrentScore"));
+            NewKillRecord();
+        }
+
+        if (PlayerPrefs.GetInt("CurrentDistance") > PlayerPrefs.GetInt("BestDistance"))
+        {
+            PlayerPrefs.SetInt("BestDistance", PlayerPrefs.GetInt("CurrentDistance"));
+            NewDistanceRecord();
+        }
+    }
+
+    void NewKillRecord()
+    {
+        KillText.transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    void NewDistanceRecord()
+    {
+        DistanceText.transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    public void SummaryButtomCall()
+    {
+        KillText.SetActive(false);
+        DistanceText.SetActive(false);
+        StartButtom.SetActive(true);
+    }
+}
+    
