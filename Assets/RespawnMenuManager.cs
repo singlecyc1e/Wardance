9631709f2efe75c1ce8fc797a7f6e0eb65d9f26b
Jiahao Ding <jiahad1@uci.edu>
2020-03-09 16:22:10 -0700

    add respawn menu

---
+++ Assets/RespawnMenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class RespawnMenuManager : MonoBehaviour
+{
+    public Animator sub1;
+    public Animator sub2;
+    public Text t1;
+
+    private void Start()
+    {
+        Time.timeScale = 1.0f;
+    }
+
+    public void LoadGame()
+    {
+        //Load next scene
+        StartCoroutine(Startgame());
+    }
+
+    IEnumerator Startgame()
+    {
+        t1.enabled = false;
+        sub1.SetTrigger("fading");
+        sub2.SetTrigger("fading");
+        yield return new WaitForSeconds(.5f);
+        SceneManager.LoadScene("RealStart");
+    }
+}
