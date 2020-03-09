c3c568443ea72f76d4e6283b9d700411ef1f60d0
Brian <bjjx1999@live.com>
2020-03-09 12:04:52 -0700

    Add stops

--- Assets/Scripts/ProgressBarController.cs
+++ Assets/Scripts/ProgressBarController.cs
@@ -2,21 +2,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class ProgressBarController : MonoBehaviour {
+    [Header("Stop road indices")]
+    public int[] stops;
+    public float stopTime;
+    
     private Slider slider;
     private int totalRoadCount;
+    private float progress;
+    private List<float> stopPercent;
+    private float counter;
+    private bool initialized;
+    private int stopIndex;
+    private bool stopped;
     
     private void Start() {
         slider = GetComponent<Slider>();
         RoadManager.instance.InitProgressBar(this);
         totalRoadCount = RoadManager.instance.GetRoadCount();
+
+        stopPercent = new List<float>();
+        for (int i = 0; i < stops.Length; i++) {
+            stopPercent.Add((float) stops[i] / totalRoadCount * (slider.maxValue - slider.minValue));
+        }
+
+        counter = 0;
+        initialized = false;
+        stopIndex = 0;
+        stopped = false;
     }
 
     public void Progress(int roadIndex) {
-        var progress = (float) roadIndex / totalRoadCount;
-        slider.value = progress;
+        progress = (float) roadIndex / totalRoadCount * (slider.maxValue - slider.minValue);
+        if (!initialized) {
+            slider.value = progress;
+            int i;
+            for (i = 0; ; i++) {
+                if(stopPercent[i] > progress) break;
+            }
+
+            stopIndex = i;
+            initialized = true;
+        }
+    }
+
+    private void Update() {
+        if (stopped) {
+            counter += Time.deltaTime;
+            if (counter >= stopTime) {
+                stopped = false;
+            }
+        } else {
+            slider.value = Mathf.MoveTowards(slider.value, progress, 0.1f * (progress - slider.value));
+            if (slider.value >= stopPercent[stopIndex]) {
+                stopped = true;
+            }
+        }
+        
     }
 }
