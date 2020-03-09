f6931f8cbc444a29b7ed5dc8d406359f76d05e46
Brian <bjjx1999@live.com>
2020-03-09 15:25:24 -0700

    Fix progress

--- Assets/Scripts/ProgressBarController.cs
+++ Assets/Scripts/ProgressBarController.cs
@@ -1,23 +1,22 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ProgressBarController : MonoBehaviour {
-    [Header("Stop road indices")]
+    [Header("Stop road indices")] 
     public int[] stops;
-    public float stopTime;
-    
+    public int stopCount;
+
     private Slider slider;
     private int totalRoadCount;
     private float progress;
     private List<float> stopPercent;
-    private float counter;
+    private int counter;
     private bool initialized;
     private int stopIndex;
+    private int savedIndex;
     private bool stopped;
-    
+
     private void Start() {
         slider = GetComponent<Slider>();
         RoadManager.instance.InitProgressBar(this);
@@ -32,15 +31,35 @@ public class ProgressBarController : MonoBehaviour {
         initialized = false;
         stopIndex = 0;
         stopped = false;
+        savedIndex = 0;
     }
 
     public void Progress(int roadIndex) {
+        if (stopped) {
+            if (roadIndex > savedIndex) {
+                ++counter;
+            }
+            
+            if (counter >= stopCount) {
+                counter = 0;
+                ++stopIndex;
+                stopped = false;
+            } else {
+                return;
+            }
+        }
+
         progress = (float) roadIndex / totalRoadCount * (slider.maxValue - slider.minValue);
+        if (stopIndex < stopPercent.Count && progress >= stopPercent[stopIndex]) {
+            savedIndex = roadIndex;
+            stopped = true;
+        }
+        
         if (!initialized) {
             slider.value = progress;
             int i;
-            for (i = 0; ; i++) {
-                if(stopPercent[i] > progress) break;
+            for (i = 0; i < stopPercent.Count; i++) {
+                if (stopPercent[i] > progress) break;
             }
 
             stopIndex = i;
@@ -49,17 +68,9 @@ public class ProgressBarController : MonoBehaviour {
     }
 
     private void Update() {
-        if (stopped) {
-            counter += Time.deltaTime;
-            if (counter >= stopTime) {
-                stopped = false;
-            }
-        } else {
-            slider.value = Mathf.MoveTowards(slider.value, progress, 0.1f * (progress - slider.value));
-            if (slider.value >= stopPercent[stopIndex]) {
-                stopped = true;
-            }
-        }
+        if (stopped) return;
+        
+        slider.value = Mathf.MoveTowards(slider.value, progress, 0.1f * (progress - slider.value));
         
     }
 }
