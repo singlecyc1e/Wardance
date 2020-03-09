f6e64a649dbc6017f00fc5d5608e64ba0c9ce771
Sicilia Miao <huizimiao@gmail.com>
2020-03-09 16:42:28 -0700

    sound effect
    alahuakeba

---
+++ Assets/Scripts/ragesound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ragesound : MonoBehaviour
+{
+    private AudioSource mysource;
+    public AudioClip myclip;
+
+    private void Awake()
+    {
+        mysource = this.GetComponent<AudioSource>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSystem.instance.Rageon.AddListener(rageeffect);
+    }
+
+    void rageeffect()
+    {
+        mysource.clip = myclip;
+        mysource.Play();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/resultsound.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resultsound : MonoBehaviour
+{
+    private AudioSource mysource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(coroutineA());
+    }
+
+    IEnumerator coroutineA()
+    {
+        // wait for 1 second
+        Debug.Log("coroutineA created");
+        yield return new WaitForSeconds(0.1f);
+        mysource.Play();
+        yield return StartCoroutine(coroutineB());
+        Debug.Log("coroutineA running again");
+    }
+
+    IEnumerator coroutineB()
+    {
+        // wait for 1 second
+        Debug.Log("coroutineA created");
+        yield return new WaitForSeconds(0.1f);
+        mysource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
