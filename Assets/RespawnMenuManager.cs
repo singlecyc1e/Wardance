c57ea66a859b56294a555c4988f57a052422955d
Sicilia Miao <huizimiao@gmail.com>
2020-03-10 02:02:47 -0700

    Redo RespawnMenu Scene

--- Assets/RespawnMenuManager.cs
+++ Assets/RespawnMenuManager.cs
@@ -11,6 +11,10 @@ public class RespawnMenuManager : MonoBehaviour
     public Animator sub2;
     public Text t1;
 
+    public GameObject Life1;
+    public GameObject Life2;
+    public GameObject Life3;
+
     private void Start()
     {
         Time.timeScale = 1.0f;
@@ -25,9 +29,15 @@ public class RespawnMenuManager : MonoBehaviour
     IEnumerator Startgame()
     {
         t1.enabled = false;
+
         sub1.SetTrigger("fading");
         sub2.SetTrigger("fading");
         yield return new WaitForSeconds(.5f);
+
+        Life1.SetActive(false);
+        Life2.SetActive(false);
+        Life3.SetActive(false);
+
         SceneManager.LoadScene("RealStart");
     }
 }
---
+++ Assets/RespawnSummary.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RespawnSummary : MonoBehaviour
+{
+    int LifeRemain;
+
+    public Text LifeCount;
+    public Text LifeText;
+    public GameObject Life2;
+    public GameObject Life3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnterScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void EnterScene()
+    {
+        LifeRemain = PlayerPrefs.GetInt(LevelController.playerLifeKey);
+        Debug.Log(LifeRemain);
+
+        LifeCount.text = LifeRemain.ToString() + "             ";
+
+        if (LifeRemain == 2)
+        {
+            Life3.GetComponent<Animator>().SetTrigger("Play");
+        }
+
+        if (LifeRemain == 1)
+        {
+            LifeText.text = "    Life Remainning";
+            Life3.SetActive(false);
+            Life2.GetComponent<Animator>().SetTrigger("Play");
+        }
+    }
+}
