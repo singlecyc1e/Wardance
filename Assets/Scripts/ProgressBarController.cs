2ea1c67cb728693369831698d599bff7f8d198cb
Brian <bjjx1999@live.com>
2020-03-10 19:23:33 -0700

    delete stops

--- Assets/Scripts/ProgressBarController.cs
+++ Assets/Scripts/ProgressBarController.cs
@@ -5,18 +5,18 @@ using UnityEngine.UI;
 public class ProgressBarController : MonoBehaviour {
     [Header("Stop road indices")] 
     public int[] stops;
-    public int stopCount;
+    // public int stopCount;
     public GameObject flag;
 
     private Slider slider;
     private int totalRoadCount;
     private float progress;
     private List<float> stopPercent;
-    private int counter;
+    // private int counter;
     private bool initialized;
     private int stopIndex;
-    private int savedIndex;
-    private bool stopped;
+    // private int savedIndex;
+    // private bool stopped;
     private RectTransform rectTransform;
     private List<GameObject> flags;
 
@@ -42,52 +42,57 @@ public class ProgressBarController : MonoBehaviour {
             flags.Add(spawned);
         }
 
-        counter = 0;
+        // counter = 0;
         initialized = false;
         stopIndex = 0;
-        stopped = false;
-        savedIndex = 0;
+        // stopped = false;
+        // savedIndex = 0;
     }
 
     public void Progress(int roadIndex) {
-        if (stopped) {
-            if (roadIndex > savedIndex) {
-                ++counter;
-                if (counter >= stopCount) {
-                    counter = 0;
-                    flags[stopIndex].GetComponent<Image>().color = Color.red;
-                    ++stopIndex;
-                    stopped = false;
-                } else {
-                    return;
-                }
-            } else {
-                return;
-            }
-        }
+        // if (stopped) {
+        //     if (roadIndex > savedIndex) {
+        //         ++counter;
+        //         if (counter >= stopCount) {
+        //             counter = 0;
+        //             flags[stopIndex].GetComponent<Image>().color = Color.red;
+        //             ++stopIndex;
+        //             stopped = false;
+        //         } else {
+        //             return;
+        //         }
+        //     } else {
+        //         return;
+        //     }
+        // }
 
         progress = (float) roadIndex / totalRoadCount * (slider.maxValue - slider.minValue);
-        if (stopIndex < stopPercent.Count && progress >= stopPercent[stopIndex]) {
-            savedIndex = roadIndex;
-            stopped = true;
+        for (int i = 0; i < stops.Length; i++) {
+            if (progress >= stopPercent[i]) {
+                flags[stopIndex].GetComponent<Image>().color = Color.red;
+            }
         }
+        // if (stopIndex < stopPercent.Count && progress >= stopPercent[stopIndex]) {
+        //     savedIndex = roadIndex;
+        //     stopped = true;
+        // }
         
         if (!initialized) {
             slider.value = progress;
             Debug.Log(slider.value);
             int i;
             for (i = 0; i < stopPercent.Count; i++) {
-                if (stopPercent[i] - progress > 0.1f) break;
+                if (stopPercent[i] - progress > 0.001f) break;
                 flags[i].GetComponent<Image>().color = Color.red;
             }
 
-            stopIndex = i;
+            stopIndex = i - 1;
             initialized = true;
         }
     }
 
     private void Update() {
-        if (stopped) return;
+        // if (stopped) return;
         
         slider.value = Mathf.MoveTowards(slider.value, progress, 0.1f * (progress - slider.value));
         
